0437c5911d4d8f5f72996409c6001c747056e307
hyxer <harith87@me.com>
2016-03-08 09:56:31 +0800

    clean
    clean unneeded compiled file

--- skript.sh
+++
@@ -1,7 +0,0 @@
-
-for zprava in "00 00 00 00  00 00 00 00" "FF FF FF FF  FF FF FF FF"; do
-   for klic in "00 00 00 00  00 00 00 00  00 00"  "FF FF FF FF  FF FF FF FF  FF FF"; 
-   do echo "$zprava" | ./present.out -k "$klic"; 
-   done
-done
-
